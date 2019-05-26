63edee22fed4880925a198cd9670bd1f8e88a1de
Jacob Lopez <jacob_m_lopez@berkeley.edu>
2019-05-26 13:10:54 -0700

    State example

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Image} from 'react-native';
+import { thisExpression } from '@babel/types';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,48 +17,32 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-class Greeting extends Component {
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isShowingText: true };
+
+    setInterval(() => (
+      this.setState(previousState => (
+        { isShowingText: !previousState.isShowingText }
+      ))
+    ), 1000);
+  }
   render() {
-    return (
-      <View style={{alignItems: 'center'}}>
-        <Text>Hello {this.props.name}!</Text>
-      </View>
-    );
+    if (!this.state.isShowingText) {
+      return null;
+    } else {
+      return (<Text>{this.props.text}</Text>);
+    }
   }
 }
 
 export default class App extends Component {
   render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Hello World!</Text>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Greeting name="Jacob"/>
-        <Greeting name="Jacob"/>
-        <Greeting name="Jacob"/>
-      </View>
+      <View>
+        <Blink text="This text will blink a lot lmao"/>
+     </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
